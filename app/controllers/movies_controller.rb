fa3c3cdd60765479b3ce5b8c53d1a6eec1665509
Michal Aimelak <michala@tailorbrands.com>
2021-12-09 17:22:26 +0200

    fetch data from a YAML file

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,35 @@
+require 'yaml'
 class MoviesController < ApplicationController
+
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
+
+
+  def cors_set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  #     render :text => '', :content_type => ''
+  end
+
   def index
-    @movies = Movie.all
+    #     @movies = Movie.all
+    #     @movies = YAML.load_file('./db/data.yml')
+    #     render json: @movies
+
+        render json: @movies = YAML.load_file(Rails.root.join("db",'movies.yml'))
   end
 
   def show
