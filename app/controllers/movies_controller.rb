da7c646e86cfe741cbec3bfd6b39b97f5ac06d4f
Michal Aimelak <michala@tailorbrands.com>
2021-12-09 15:03:58 +0200

    add CRUD interface for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,6 +6,47 @@ class MoviesController < ApplicationController
   def show
      @movie = Movie.find(params[:id])
   end
+
+   def new
+      @movie = Movie.new
+    end
+
+  def create
+     @movie = Movie.new(movie_params)
+
+     if @movie.save
+       redirect_to @movie
+     else
+       render :new
+     end
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+
+    redirect_to root_path
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :body)
+    end
+
 end
 
 
